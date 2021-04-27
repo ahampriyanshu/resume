45fcc01aa85fc5463aacf35d2334a3b9e2e43bee
ahampriyanshu <ahampriyanshu@gmail.com>
2021-04-27 10:13:03 +0530

    restarting repo

---
+++ README.md
@@ -0,0 +1,39 @@
+## Cyptography
+
+## Task 0
+
+* To implement ``Shamir Secret Sharing Alogithm`` using C.
+
+<details>
+<summary>Output</summary>
+
+![task 1](screenshots/1.png)
+
+</details>
+## Task 1
+
+* To understand how ``make`` works and then to calculate a ``BIP39 Seed`` using 24 bip mnemonics.
+
+<details>
+<summary>Output</summary>
+
+![task 2](screenshots/2.png)
+
+</details>
+
+## Task 2
+
+* To derive ``public key``, ``private key``, ``chain node`` and ``reciever and change address`` for ``Bitcoin Testnet`` using 24 bip mnemonics for derivation path ``m/44'/1'/0`/0/``
+
+<details>
+<summary>Output</summary>
+
+![task 3](screenshots/3.png)
+
+</details>
+
+## Resources
+
+* [https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki](https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki)
+* [https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki](https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki)
+* [https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki](https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki)
--- task1/main.c
+++ task1/main.c
@@ -10,8 +10,8 @@
 // Do display ongoing porcess
 static void progress_callback(uint32_t pos, uint32_t len)
 {
-	printf("%" PRIu32 " / ", pos);
-	printf("%" PRIu32 " ... \n", len);
+	printf("%" PRIu32 "/", pos);
+	printf("%" PRIu32 " ... ", len);
 }
 
 // Driver Code
@@ -19,19 +19,19 @@ int main(void)
 {
 	const char *passphrase = "";
 	int seedlength = 64;
-	const char *mnemonic = "garden reject beauty inch scissors rifle amazing couch bacon multiply swim poverty impose spray ugly term stamp prevent nothing mutual awful project wrist movie";
+	const char *mnemonic = "garden reject beauty inch scissor rifle amazing couch bacon multiply swim poverty impose spray ugly term stamp prevent nothing mutual awful project wrist movie";
 	uint8_t seed[seedlength];
 
-	printf("Mnemonic : %s\nGenerating Seed ... \n", mnemonic);
+	printf("\nBIP Mnemonic :\n%s\nGenerating Seed ... \n", mnemonic);
 
 	mnemonic_to_seed(mnemonic, passphrase, seed, progress_callback);
 
-	printf("Your seed is : ");
+	printf("\nYour seed is : ");
 	for (size_t i = 0; i < seedlength; i++)
 	{
 		printf("%02x", seed[i]);
 	}
-	printf("\n");
+	printf("\n\n");
 
 	return 0;
 }
